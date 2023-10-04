9b3e2292e70dfd6f32367c1aa44b457f66450776
Khalil <khalkhool1999@gmail.com>
2023-10-04 10:51:45 +0300

    final edits

--- logindb.sql
+++ logindb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2023 at 04:45 PM
+-- Generation Time: Oct 04, 2023 at 09:46 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
